d834489424d30d1692e93036a576eb7377b8765b
ShadyWestHaze <zakatistovu@gmail.com>
2024-07-02 17:07:12 +0300

    nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,15 @@
-import Link from 'next/link';
-import Button from '@/components/button';
 import React from 'react';
-import { options } from "./api/auth/[...nextauth]/options"
 import { getServerSession } from 'next-auth/next';
+import { options } from "./api/auth/[...nextauth]/options";
 import '/app/global.css';
 
 export default async function Home() {
-    const session = await getServerSession(options);
-    
+
+
     return (
         <>
-        
-              <h1 >Home Page</h1>
+          
             
-            {session ? (
-                <>
-                    <Button text="Payments" href="/payments" />
-                    <Button text="Logout" href="api/auth/signout" />
-                </>
-            ) : (
-                <>
-                <Button text="Login" href="api/auth/signin" />
-                <Button text="Sign Up" href="api/signup" />
-                </>
-            )}
         </>
     );
 }
